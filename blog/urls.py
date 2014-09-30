3843137ab8962abeab717441927cbbcb765f0cc4
Alex Dobrushskiy <Alexey.Dobrushskiy@artezio.com>
2014-09-30 08:43:44 +0400

    Adding blog post from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
-from blog.views import simple_view
+from blog.views import simple_view, AddPost
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'application.views.home', name='home'),
     url(r'^/?$', simple_view),
+    url(r'^add/?$', AddPost.as_view()),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from blog.models import Post
 from django.shortcuts import render
+from django.views.generic import View
 
 
 def simple_view(request):
     all_posts = Post.objects.all()
 
     return HttpResponse(render(request, 'blog/all_records.html', {'posts': all_posts}))
+
+
+class AddPost(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(render(request, 'blog/add_post.html', {}))
+
+    def post(self, request, *args, **kwargs):
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if title and content:
+            Post.objects.create(title=title, content=content)
+
+            return HttpResponse('Blog post "%s" succesfully added' % title)
+        else:
+            return HttpResponseBadRequest('Title and content shoulnd be empty!')
